6af661d7267c4a16af5e6ad8b7530ba9c15e86a4
Antreas Papadopoulos <antreas@ccllhr.com>
2022-05-26 23:16:03 +0100

    Use of refs to access value of input fields instead of useState

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 
 import Card from '../UI/Card';
 import Button from '../UI/Button';
@@ -14,16 +14,19 @@ const AddUser = (props) => {
     The first will hold the actual content where are the 2nd 'use...' is the function
     called to update said state adn trigger a re-render
   */}
-  const [enteredUsername, setEnteredUsername] = useState('');
-  const [enteredAge, setEnteredAge] = useState('');
+  const nameInputRef = useRef();
+  const ageInputRef = useRef();
+
   const [error, setError] = useState();
 
   const addUserHandler = (event) => {
     {/* Stop the form from submitting */}
     event.preventDefault();
-    console.log(enteredUsername, enteredAge);
+    const enteredName = nameInputRef.current.value;
+    const enteredUserAge = ageInputRef.current.value;
+
     {/* Simple form Validation */}
-    if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+    if (enteredName.trim().length === 0 || enteredUserAge.trim().length === 0) {
       setError({
         title: 'Invalid input',
         message: 'Please enter a valid name and age (non-empty values).',
@@ -31,25 +34,17 @@ const AddUser = (props) => {
       return;
     }
     {/*Even though age is a string it should work even if I don't add the + at the start but i do it just to be extra safe*/}
-    if (+enteredAge < 1) {
+    if (+enteredUserAge < 1) {
       setError({
         title: 'Invalid age',
         message: 'Please enter a valid age (> 0).',
       });
       return;
     }
-    props.onAddUser(enteredUsername, enteredAge);
-    {/*Empty the input fields on form submission*/}
-    setEnteredUsername('');
-    setEnteredAge('');
-  };
-
-  const usernameChangeHandler = (event) => {
-    setEnteredUsername(event.target.value);
-  };
-
-  const ageChangeHandler = (event) => {
-    setEnteredAge(event.target.value);
+    props.onAddUser(enteredName, enteredUserAge);
+    {/*Ideally you do not manipulate dom elements outside of React but in the case of clearing input values it's fine*/}
+    nameInputRef.current.value = '';
+    ageInputRef.current.value = '';
   };
 
   const errorHandler = () => {
@@ -70,20 +65,16 @@ const AddUser = (props) => {
         {/*We don't do addUserHandler() cause that would just execute the function and we don't want that. Instead we simply point to it!*/}
         <form onSubmit={addUserHandler}>
           <label htmlFor="username">Username</label>
-          {/*On change we will call the usernameChangeHandler which in turn calls the setEnteredUsername which will update the username entered on every key stroke*/}
           <input
             id="username"
             type="text"
-            value={enteredUsername}
-            onChange={usernameChangeHandler}
+            ref={nameInputRef}
           />
           <label htmlFor="age">Age (Years)</label>
-          {/*On change we will call the ageChangeHandler which in turn calls the setEnteredAge which will update the age entered on every key stroke*/}
           <input
             id="age"
             type="number"
-            value={enteredAge}
-            onChange={ageChangeHandler}
+            ref={ageInputRef}
           />
           <Button type="submit">Add User</Button>
         </form>
